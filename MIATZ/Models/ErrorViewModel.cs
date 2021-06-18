a4172f9616d29fd7915262cd1d6cc4ec3efdc0a5
hw2cb <dim5.dim5.960@mail.ru>
2021-06-18 20:56:27 +0400

    Добавьте файлы проекта.

---
+++ MIATZ/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MIATZ.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
