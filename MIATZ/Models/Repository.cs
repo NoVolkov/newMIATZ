a4acf49775839ea47832e69c7982c4e3f7902e70
hw2cb <dim5.dim5.960@mail.ru>
2021-06-19 21:05:15 +0400

    update db

--- MIATZ/Models/Repository.cs
+++ MIATZ/Models/Repository.cs
@@ -7,9 +7,9 @@ namespace MIATZ.Models
 {
     public static class Repository
     {
-        public static string connectionString = @"Data Source=192.168.66.191,49674;Initial Catalog=miac;Persist Security Info=True;User ID=USER;Password=123456";
+        //public static string connectionString = @"Data Source=192.168.66.191,49674;Initial Catalog=miac;Persist Security Info=True;User ID=USER;Password=123456";
 
-        //public static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\dim5d\source\repos\MIATZ\MIATZ\AppData\miazDB.mdf;Integrated Security=True;Connect Timeout=30";
+        public static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\dim5d\source\repos\MIATZ\MIATZ\AppData\miazDB.mdf;Integrated Security=True;Connect Timeout=30";
         //public staticc string connectionString { get; set; }
     }
 }
